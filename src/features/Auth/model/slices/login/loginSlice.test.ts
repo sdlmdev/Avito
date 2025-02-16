0f198a3ca77cc650d9b4677397163324ed2ec9a7
sdlmdev <sdlmdev@yandex.ru>
2025-02-16 22:46:54 +0300

    fix: открыл тест

--- src/features/Auth/model/slices/login/loginSlice.test.ts
+++ src/features/Auth/model/slices/login/loginSlice.test.ts
@@ -1,23 +1,23 @@
-// import {LoginScheme} from '../../types/loginScheme';
-// import {loginActions, loginReducer} from './loginSlice';
-//
-// describe('Тесты loginSlice', () => {
-//   test('Должен установить username', () => {
-//     const state: DeepPartial<LoginScheme> = {username: '123'};
-//
-//     expect(
-//       loginReducer(state as LoginScheme, loginActions.setUsername('testName')),
-//     ).toEqual({username: 'testName'});
-//   });
-//
-//   test('Должен установить password', () => {
-//     const state: DeepPartial<LoginScheme> = {password: '123'};
-//
-//     expect(
-//       loginReducer(
-//         state as LoginScheme,
-//         loginActions.setPassword('testPassword'),
-//       ),
-//     ).toEqual({password: 'testPassword'});
-//   });
-// });
+import {LoginScheme} from '../../types/loginScheme';
+import {loginActions, loginReducer} from './loginSlice';
+
+describe('Тесты loginSlice', () => {
+  test('Должен установить username', () => {
+    const state: DeepPartial<LoginScheme> = {username: '123'};
+
+    expect(
+      loginReducer(state as LoginScheme, loginActions.setUsername('testName')),
+    ).toEqual({username: 'testName'});
+  });
+
+  test('Должен установить password', () => {
+    const state: DeepPartial<LoginScheme> = {password: '123'};
+
+    expect(
+      loginReducer(
+        state as LoginScheme,
+        loginActions.setPassword('testPassword'),
+      ),
+    ).toEqual({password: 'testPassword'});
+  });
+});
